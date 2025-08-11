b0a18930f4abd9c4b95e9f07059bb26b0dc3dac2
lebensquelleplus <156298781+lebensquelleplus@users.noreply.github.com>
2025-08-11 16:36:13 +0200

    Create export_config.py

---
+++ config/export_config.py
@@ -0,0 +1,18 @@
+
+
+class ExportConfig:
+    """Export-Konfiguration"""
+
+    # Unterstützte Formate
+    SUPPORTED_FORMATS = ['csv', 'excel', 'json', 'pdf']
+
+    # Standard-Dateiname Template
+    FILENAME_TEMPLATE = "stornierungsanfragen_{timestamp}"
+
+    # CSV Einstellungen
+    CSV_SEPARATOR = ','
+    CSV_ENCODING = 'utf-8-sig'  # BOM für Excel
+
+    # Excel Einstellungen
+    EXCEL_SHEET_NAME = 'Stornierungsanfragen'
+    EXCEL_AUTO_FILTER = True
